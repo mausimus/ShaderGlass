354fcbd6a9cca547ab80d0c110008b86aa237bee
rohatsu <rohatsu@users.noreply.github.com>
2021-10-28 20:30:04 +0900

    custom aspect

--- ShaderGlass/resource.h
+++ ShaderGlass/resource.h
@@ -15,6 +15,8 @@
 #define IDM_PIXELSIZE_NEXT              111
 #define IDM_UPDATE_PARAMS               112
 #define IDR_MAINFRAME                   128
+#define IDD_INPUT_DIALOG                129
+#define IDC_EDIT1                       1000
 #define ID_FILE_START                   32771
 #define IDM_FILE_START                  32772
 #define IDM_START                       32773
@@ -126,15 +128,16 @@
 #define ID_SHADER_PARAMETERS            32887
 #define IDM_SHADER_PARAMETERS           32888
 #define IDC_STATIC                      -1
+#define IDC_STATIC_LABEL                -1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32889
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           113
 #endif
 #endif
