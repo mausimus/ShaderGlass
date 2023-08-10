0b5e771b913a336e9812721b3b8dba42a79d7440
mausimus <73635663+mausimus@users.noreply.github.com>
2023-08-10 21:13:58 +0900

    default shader chg

--- ShaderGlass/ShaderWindow.cpp
+++ ShaderGlass/ShaderWindow.cpp
@@ -1511,7 +1511,7 @@ bool ShaderWindow::Create(_In_ HINSTANCE hInstance, _In_ int nCmdShow)
     // set defaults
     SendMessage(m_mainWindow, WM_COMMAND, WM_PIXEL_SIZE(2), 0);
     SendMessage(m_mainWindow, WM_COMMAND, WM_ASPECT_RATIO(0), 0);
-    SendMessage(m_mainWindow, WM_COMMAND, WM_SHADER(39), 0);
+    SendMessage(m_mainWindow, WM_COMMAND, WM_SHADER(54), 0);
     SendMessage(m_mainWindow, WM_COMMAND, WM_FRAME_SKIP(1), 0);
     SendMessage(m_mainWindow, WM_COMMAND, WM_OUTPUT_SCALE(0), 0);
     SendMessage(m_mainWindow, WM_COMMAND, WM_CAPTURE_DISPLAY(0), 0);
